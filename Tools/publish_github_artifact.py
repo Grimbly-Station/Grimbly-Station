11bbc51d660cb90e6d2695b589bf33195a98823d
flyingkarii <123355664+flyingkarii@users.noreply.github.com>
2024-09-05 16:43:07 -0300

    Change fork URL and ID
    Signed-off-by: flyingkarii <123355664+flyingkarii@users.noreply.github.com>

--- Tools/publish_github_artifact.py
+++ Tools/publish_github_artifact.py
@@ -14,8 +14,8 @@ VERSION = os.environ['GITHUB_SHA']
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://cdn.delta-v.org/"
-FORK_ID = "delta-v"
+ROBUST_CDN_URL = "https://cdn.grimbly.net/"
+FORK_ID = "grimbly"
 
 def main():
     print("Fetching artifact URL from API...")
